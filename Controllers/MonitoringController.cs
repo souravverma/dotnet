27650e2ed7d05a2fdd15b5b705a83e9f205546f9
souravverma <souravverma@gmail.com>
2022-04-15 13:56:00 +0530

    Add files via upload

---
+++ Controllers/MonitoringController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelemetryLibrary;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitoringController : Controller
+    {
+        private readonly ILogger<MonitoringController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MonitoringController(ILogger<MonitoringController> logger, IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+
+        }
+
+       // [HttpGet("Monitoring")]
+
+       [Route("{name}/{path}")]
+        public string Get(string name, string path)
+        {
+            TelemetryService objTelService = new TelemetryService(name, path, _httpContextAccessor);
+            
+            string jsonData = objTelService.getMetrics();
+
+            return jsonData;
+
+        }
+
+        [Route("{*url}", Order = 999)]
+        public IActionResult CatchAll()
+        {
+            return new ContentResult()
+            {
+                StatusCode = 404,
+                Content = "Not found"
+            };
+        }
+    }
+}
